6f1c6921ec1ae700b5f01b34f78e8a953e8230e7
mafu <mafusukee@gmail.com>
2025-07-29 22:09:49 +0900

    Implemented feature_statistics.py

---
+++ src/misc/feature_statistics.py
@@ -0,0 +1,25 @@
+import pyprojroot
+import pickle
+import numpy as np
+
+base_path = pyprojroot.find_root(pyprojroot.has_dir(".git"))
+
+path = base_path / "data/features/train.pkl"
+
+with open(path, "rb") as f:
+    s, y, X = pickle.load(f)
+    
+# n番目の特徴量の値を分析
+# 2202: 機能語の使用頻度「特に」
+# 2142: 句読点の分布(8)
+feature_num = 2142
+chatgpt4o = []
+wiki40b = []
+for i in range(len(y)):
+    if y[i] == 1:
+        chatgpt4o.append(X[i, feature_num])
+    else:
+        wiki40b.append(X[i, feature_num])
+        
+# 平均値を計算
+print(np.mean(chatgpt4o), np.mean(wiki40b))
